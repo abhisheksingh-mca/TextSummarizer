bd47e515394935eba7014f126a2c51567a279e3c
Abhishek <onlyabk@gmail.com>
2024-01-11 23:45:57 +0530

    remove useless line 9

--- text.py
+++ text.py
@@ -6,7 +6,6 @@ import re
 import nltk 
 nltk.download('punkt')
 nltk.download('stopwords')
-from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 import heapq
